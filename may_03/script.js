641469ec142fce3509b0e4505af8fa421c450585
Mojito <vu3xk41997@gmail.com>
2022-05-03 15:41:44 +0200

    challenge done

---
+++ may_03/script.js
@@ -0,0 +1,24 @@
+// 題目一：hello world！
+function Hello() {
+    return "Hello, world!"
+}
+
+// 題目二：參數+字串練習
+function sayHi(name) { 
+    return name + ", hello world!"
+}
+
+// 題目三：參數加法產生器
+function add(num) {
+    return num + num
+}
+
+// 題目四：兩個參數相加器
+function addTwo(num1, num2) {
+    return num1 + num2
+}
+
+// 題目五：參數+字串相加
+function addNumberAndString(num1, num2, num3) {
+    return "第一加第二個參數加總為 " + addTwo(num1, num2) + "，第三個參數為 " + num3
+}
