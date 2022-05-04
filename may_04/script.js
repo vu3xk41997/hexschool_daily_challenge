c70899323ec6721a1e47dfd38ed9d2ec4ca6b245
Mojito <vu3xk41997@gmail.com>
2022-05-04 13:03:48 +0200

    daily challenge done

---
+++ may_04/script.js
@@ -0,0 +1,24 @@
+// 題目一：平方計算
+function square(num) {
+    return num * num
+}
+
+// 題目二：數學邏輯題
+function calculate(num1, num2, num3) {
+    return num1 + num2 - num3
+}
+
+// 題目三：四捨五入題
+function rounding(num) {
+    return num.toFixed(2)
+}
+
+// 題目四：字串轉數字
+function parseNumber(string) {
+    return parseFloat(string)
+}
+
+// 題目五：被二整除
+function divisible(num) {
+    return num % 2
+}
