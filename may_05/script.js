95981d6a3e461740246697190c0f3489b8393b4f
Mojito <vu3xk41997@gmail.com>
2022-05-05 13:42:00 +0200

    challenge done

---
+++ may_05/script.js
@@ -0,0 +1,24 @@
+// 題目一：字串長度計算
+function checkStringLength(string) {
+    return string.length
+}
+
+// 題目二：英文強制小寫
+function lowerCase(string) {
+    return string.toLowerCase()
+}
+
+// 題目三：去除掉段落的左右空白
+function trimString(string) {
+    return string.trim()
+}
+
+// 題目四：字串依照條件轉陣列
+function splitString(string) {
+    return string.split(',')
+}
+
+// 題目五：切割市區字串
+function getArea(string) {
+    return string.slice(3,6)
+}
